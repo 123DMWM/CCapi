ca9adbd5e466c55d15586a0b0db489fa49618913
123DMWM <shmo1joe2@gmail.com>
2015-06-14 20:29:36 -0400

    Initial Commit

---
+++ CCapi/Form1.Designer.cs
@@ -0,0 +1,452 @@
+﻿namespace CCapi {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox24 = new System.Windows.Forms.TextBox();
+			this.textBox23 = new System.Windows.Forms.TextBox();
+			this.textBox21 = new System.Windows.Forms.TextBox();
+			this.textBox19 = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.textBox17 = new System.Windows.Forms.TextBox();
+			this.textBox15 = new System.Windows.Forms.TextBox();
+			this.textBox13 = new System.Windows.Forms.TextBox();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.tabPage2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(12, 12);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(576, 270);
+			this.tabControl1.TabIndex = 1;
+			this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_Change);
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.label11);
+			this.tabPage1.Controls.Add(this.label10);
+			this.tabPage1.Controls.Add(this.label9);
+			this.tabPage1.Controls.Add(this.label8);
+			this.tabPage1.Controls.Add(this.button4);
+			this.tabPage1.Controls.Add(this.textBox24);
+			this.tabPage1.Controls.Add(this.textBox23);
+			this.tabPage1.Controls.Add(this.textBox21);
+			this.tabPage1.Controls.Add(this.textBox19);
+			this.tabPage1.Controls.Add(this.panel1);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.textBox1);
+			this.tabPage1.Controls.Add(this.pictureBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(568, 244);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Players";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(399, 7);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(29, 23);
+			this.button4.TabIndex = 14;
+			this.button4.Text = "ID";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// textBox24
+			// 
+			this.textBox24.Location = new System.Drawing.Point(234, 86);
+			this.textBox24.Name = "textBox24";
+			this.textBox24.ReadOnly = true;
+			this.textBox24.Size = new System.Drawing.Size(134, 20);
+			this.textBox24.TabIndex = 10;
+			// 
+			// textBox23
+			// 
+			this.textBox23.Location = new System.Drawing.Point(234, 190);
+			this.textBox23.Multiline = true;
+			this.textBox23.Name = "textBox23";
+			this.textBox23.ReadOnly = true;
+			this.textBox23.Size = new System.Drawing.Size(328, 45);
+			this.textBox23.TabIndex = 9;
+			// 
+			// textBox21
+			// 
+			this.textBox21.Location = new System.Drawing.Point(234, 138);
+			this.textBox21.Name = "textBox21";
+			this.textBox21.ReadOnly = true;
+			this.textBox21.Size = new System.Drawing.Size(328, 20);
+			this.textBox21.TabIndex = 7;
+			// 
+			// textBox19
+			// 
+			this.textBox19.Location = new System.Drawing.Point(374, 86);
+			this.textBox19.Name = "textBox19";
+			this.textBox19.ReadOnly = true;
+			this.textBox19.Size = new System.Drawing.Size(54, 20);
+			this.textBox19.TabIndex = 5;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label7);
+			this.panel1.Controls.Add(this.label6);
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.Controls.Add(this.textBox4);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Location = new System.Drawing.Point(6, 6);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(222, 232);
+			this.panel1.TabIndex = 3;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(144, 1);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Refresh";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(4, 29);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(215, 174);
+			this.textBox4.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(160, 209);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(59, 20);
+			this.textBox2.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(340, 7);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(53, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Lookup Player/ID";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(234, 8);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "Player Name/ID";
+			this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_Key);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(434, 6);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.label5);
+			this.tabPage2.Controls.Add(this.label4);
+			this.tabPage2.Controls.Add(this.label3);
+			this.tabPage2.Controls.Add(this.label2);
+			this.tabPage2.Controls.Add(this.label1);
+			this.tabPage2.Controls.Add(this.textBox17);
+			this.tabPage2.Controls.Add(this.textBox15);
+			this.tabPage2.Controls.Add(this.textBox13);
+			this.tabPage2.Controls.Add(this.textBox9);
+			this.tabPage2.Controls.Add(this.textBox7);
+			this.tabPage2.Controls.Add(this.button2);
+			this.tabPage2.Controls.Add(this.comboBox1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(568, 244);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Servers";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// textBox17
+			// 
+			this.textBox17.Location = new System.Drawing.Point(485, 59);
+			this.textBox17.Name = "textBox17";
+			this.textBox17.ReadOnly = true;
+			this.textBox17.Size = new System.Drawing.Size(77, 20);
+			this.textBox17.TabIndex = 13;
+			// 
+			// textBox15
+			// 
+			this.textBox15.Location = new System.Drawing.Point(6, 163);
+			this.textBox15.Name = "textBox15";
+			this.textBox15.ReadOnly = true;
+			this.textBox15.Size = new System.Drawing.Size(556, 20);
+			this.textBox15.TabIndex = 11;
+			// 
+			// textBox13
+			// 
+			this.textBox13.Location = new System.Drawing.Point(6, 111);
+			this.textBox13.Name = "textBox13";
+			this.textBox13.ReadOnly = true;
+			this.textBox13.Size = new System.Drawing.Size(556, 20);
+			this.textBox13.TabIndex = 9;
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(370, 59);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.ReadOnly = true;
+			this.textBox9.Size = new System.Drawing.Size(68, 20);
+			this.textBox9.TabIndex = 5;
+			this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(6, 59);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.ReadOnly = true;
+			this.textBox7.Size = new System.Drawing.Size(358, 20);
+			this.textBox7.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(487, 6);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Refresh";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(6, 6);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(475, 21);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(7, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 15;
+			this.label1.Text = "Name";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(7, 92);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(40, 13);
+			this.label2.TabIndex = 16;
+			this.label2.Text = "Uptime";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(7, 144);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(49, 13);
+			this.label3.TabIndex = 17;
+			this.label3.Text = "Software";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(367, 40);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(67, 13);
+			this.label4.TabIndex = 18;
+			this.label4.Text = "Player Count";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(444, 62);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(34, 13);
+			this.label5.TabIndex = 19;
+			this.label5.Text = "out of";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(1, 13);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(98, 13);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "Last Five Accounts";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(75, 212);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(79, 13);
+			this.label7.TabIndex = 6;
+			this.label7.Text = "Total Accounts";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(231, 70);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(60, 13);
+			this.label8.TabIndex = 15;
+			this.label8.Text = "User Name";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(371, 70);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(18, 13);
+			this.label9.TabIndex = 16;
+			this.label9.Text = "ID";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(231, 122);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(58, 13);
+			this.label10.TabIndex = 17;
+			this.label10.Text = "Registered";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(231, 174);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(32, 13);
+			this.label11.TabIndex = 18;
+			this.label11.Text = "Flags";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(600, 294);
+			this.Controls.Add(this.tabControl1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "ClassiCube API";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox15;
+		private System.Windows.Forms.TextBox textBox13;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox17;
+		private System.Windows.Forms.TextBox textBox24;
+		private System.Windows.Forms.TextBox textBox23;
+		private System.Windows.Forms.TextBox textBox21;
+		private System.Windows.Forms.TextBox textBox19;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ CCapi/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ServiceStack.Text;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CCapi {
+	public partial class Form1 : Form {		
+		public Form1() {
+			InitializeComponent();
+			getLastFive();
+		}
+		#region Players
+		private void button1_Click(object sender, EventArgs e) {
+			getPlayer(1);
+		}
+
+		private void button4_Click(object sender, EventArgs e) {
+			getPlayer(2);
+		}
+		private void textBox1_Key(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.Enter) {
+				getPlayer(3);
+			}
+		}
+		private void getPlayer(byte function) {
+			string name = textBox1.Text;
+			var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+			JsonObject result = null;
+			string api;
+			if (function == 2) {
+				api = "id/";
+				int id;
+				if (!int.TryParse(name, out id)) {
+					MessageBox.Show("That is not a valid ID!");
+					return;
+				}
+			} else {
+				api = "player/";
+			}
+			bool retern = false;
+			switch (function) {
+				case 1:
+				case 2:
+					try {
+						result = JsonObject.Parse((new WebClient()).DownloadString("http://www.classicube.net/api/" + api + name));
+						if (("User not found").Equals(result.Get("error"))) {
+							MessageBox.Show("No player found by that Name or ID!");
+							retern = true;
+							break;
+						}
+					} catch {
+						MessageBox.Show("ClassiCube.net might be down!");
+						retern = true;
+						break;
+					}
+					break;
+				case 3:
+					try {
+						result = JsonObject.Parse((new WebClient()).DownloadString("http://www.classicube.net/api/player/" + name));
+					} catch {
+						MessageBox.Show("ClassiCube.net might be down!");
+						retern = true;
+						break;
+					}
+					if (result.Get("error").Equals("User not found")) {
+						result = JsonObject.Parse((new WebClient()).DownloadString("http://www.classicube.net/api/id/" + name));
+						if (result.Get("error").Equals("User not found")) {
+							MessageBox.Show("No player found by that Name or ID!");
+							retern = true;
+							break;
+						}
+					}
+					break;
+				default:
+					break;
+			}
+			if (retern)
+				return;
+
+			textBox24.Text = result.Get("username");
+			textBox19.Text = result.Get("id");
+			DateTime registered = epoch.AddSeconds(double.Parse(result.Get("registered"))).ToLocalTime();
+			textBox21.Text = registered.ToLongDateString() + " at " + registered.ToLongTimeString();
+			string flagsResult = result.Get("flags");
+			string flags = "ClassiCube User, ";
+			if (flagsResult.Contains('b')) {
+				flags += "Banned from forums, ";
+			}
+			if (flagsResult.Contains('d')) {
+				flags += "ClassiCube Devloper, ";
+			}
+			if (flagsResult.Contains('m')) {
+				flags += "Forum Moderator, ";
+			}
+			if (flagsResult.Contains('a')) {
+				flags += "Forum Administrator, ";
+			}
+			if (flagsResult.Contains('e')) {
+				flags += "ClassiCube Blog Editor, ";
+			}
+			textBox23.Text = flags.Remove(flags.Length - 2, 2);
+			pictureBox1.Image = getAvatar(result.Get("username"));
+		}
+		private Image getAvatar(string name) {
+			WebClient client = new WebClient();
+			Stream stream;
+			try {
+				stream = client.OpenRead("http://www.classicube.net/face/" + name);
+				return Image.FromStream(stream);
+			} catch {
+				MessageBox.Show("Failed to get skin. ClassiCube.net might be down!");
+				return null;
+			}
+
+		}
+
+		private void getLastFive() {
+			JsonObject result = null;			
+			Regex nan = new Regex("[^a-zA-Z0-9_.,]");
+			try {
+				result = JsonObject.Parse((new WebClient()).DownloadString("http://www.classicube.net/api/players"));
+				textBox4.Text = nan.Replace(result.Get("lastfive"), "").Replace(",", Environment.NewLine);
+				textBox2.Text = result.Get("playercount");
+			} catch {
+				MessageBox.Show("ClassiCube.net might be down!");
+				return;
+			}
+		}
+		private void button3_Click(object sender, EventArgs e) {
+			getLastFive();
+		}
+		#endregion
+		#region Servers
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+			getServerInfo();
+		}
+		private void tabControl1_Change(object sender, EventArgs e) {
+			if (tabControl1.TabIndex == 1) {
+				getServers();
+			}
+		}
+		private void button2_Click(object sender, EventArgs e) {
+			getServers();
+		}
+		List<ccServer> servers;
+		private void getServers() {
+			string content = new StreamReader(new WebClient().OpenRead("http://www.classicube.net/api/serverlist")).ReadToEnd();
+			servers = JsonConvert.DeserializeObject<List<ccServer>>(content);
+			comboBox1.Items.Clear();
+			textBox7.Text = "";
+			textBox9.Text = "";
+			textBox17.Text = "";
+			textBox13.Text = "";
+			textBox15.Text = "";
+			for (int i = 0; i < servers.Count; i++) {
+				comboBox1.Items.Add(servers[i].name);
+			}
+		}
+		private void getServerInfo() {
+			textBox7.Text = servers[comboBox1.SelectedIndex].name;
+			textBox9.Text = servers[comboBox1.SelectedIndex].players;
+			textBox17.Text = servers[comboBox1.SelectedIndex].maxplayers;
+			textBox13.Text = timeToString(TimeSpan.FromSeconds(double.Parse(servers[comboBox1.SelectedIndex].uptime)));
+			textBox15.Text = servers[comboBox1.SelectedIndex].software;
+			return;
+		}
+		private string timeToString(TimeSpan span) {
+			if (span.TotalSeconds < 60) {
+				return String.Format("{0} seconds", span.Seconds);
+			} else if (span.TotalMinutes < 60) {
+				return String.Format("{0} minutes {1} seconds", span.Minutes, span.Seconds);
+			} else if (span.TotalHours < 48) {
+				return String.Format("{0} hours {1} minutes", (int)Math.Floor(span.TotalHours), span.Minutes);
+			} else if (span.TotalDays < 15) {
+				return String.Format("{0} days {1} hours", span.Days, span.Hours);
+			} else {
+				return String.Format("{0:0} weeks {1:0} days", Math.Floor(span.TotalDays / 7), Math.Floor(span.TotalDays) % 7);
+			}
+		}
+
+	}
+	class ccServer {
+		public String hash { get; set; }
+		public String maxplayers { get; set; }
+		public String name { get; set; }
+		public String players { get; set; }
+		public String software { get; set; }
+		public String uptime { get; set; }
+	}
+		#endregion
+}
